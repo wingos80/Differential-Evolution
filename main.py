2bdb8d1ab4ded4e6189865e92b3f0f162c65cc77
wingos80 <wingyc80@gmail.com>
2023-05-07 16:29:59 +0200

    test with new tournament selection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import timeit
 from scipy.spatial import ConvexHull
 import numpy as np
 import matplotlib.pyplot as plt
-np.random.seed(7)
+# np.random.seed(7)
 g = 200000
 n = 200
 x_dim, y_dim = 6, 6
@@ -132,7 +132,16 @@ def select(population, trial_vectors):
     tournament_size = 35
     selection = np.zeros_like(population)
 
-    # Your code here
+    # Tournament selection
+    # for _ in range(2):
+    #     temp = selection_pool
+    #     for i in range(int(2*len(population)/tournament_size)):
+    #         match = np.random.choice(np.arange(0,int(len(temp))), tournament_size,replace=False)
+    #         match_fitness = [pool_fitness[j] for j in match]
+    #         winner = match[np.argmax(match_fitness)]
+    #         # selection.append(temp[winner])
+    #         selection[i+_*int(2*len(population)/tournament_size)] = temp[winner]
+    #         temp = np.delete(temp, match, axis=0)
     for i in range (int(len(population))):
         # print(f'shape of selection pool and fitness: {selection_pool.shape}, {pool_fitness.shape}')
         match = np.random.choice(np.arange(0,int(selection_pool.shape[0])), tournament_size, replace=False)
@@ -147,11 +156,6 @@ def select(population, trial_vectors):
 particles = dims/3*(np.random.rand(n,dims.shape[0]) - 0.5)-10
 particles = np.ones((n,2))*-1000
 particles[:,0] = particles[:,0]
-x = np.linspace(-5000,5000,600)
-y = np.linspace(-5000,5000,500)
-
-X, Y = np.meshgrid(x,y)
-Z = f(X,Y)
 
 # make list to keep track of centroid of particles
 centroids = np.zeros((g,2))
@@ -163,6 +167,14 @@ areas = np.zeros((g,2))
 rho = 0.95 # Rho value for smoothing
 s_prev = 0 # Initial value ewma value
 s_prev1 = 1 # Initial value ewma value
+
+xmax, xmin = particles[:,0].max(), particles[:,0].min()
+ymax, ymin = particles[:,1].max(), particles[:,1].min()
+x = np.linspace(-5000,5000,350)
+y = np.linspace(-5000,5000,350)
+
+X, Y = np.meshgrid(x,y)
+Z = f(X,Y)
 
 tic = timeit.default_timer()
 for i in range(g):
@@ -206,9 +218,9 @@ for i in range(g):
     # Append new smoothed value to array
     s_prev = s_cur
     centroids[i,:] = s_cur_bc.reshape(1,2)
-
     # visualize the location of particeles on a 2d scatter plot, draw contours of the function f(x,y) over the same scatter plot
     plt.figure(1)
+    
     plt.scatter(particles[:,0], particles[:,1], c='r',s=2,zorder=2)
     plt.scatter(centroids[i][0], centroids[i][1], c='b',s=3,zorder=3)
     plt.contour(X,Y,Z, 20,zorder=1)
@@ -224,8 +236,9 @@ toc = timeit.default_timer()
 print(f'\n\n\n************************************************************\n')
 print(f'Finished optimizing\n\n\nTotal time: {round(toc-tic,3)}s')
 
-x = np.linspace(-20000,20000,2000)
-y = np.linspace(-20000,20000,2000)
+mins, maxs = 1500, 2222
+x = np.linspace(mins,maxs,2000)
+y = np.linspace(mins,maxs,2000)
 
 
 X, Y = np.meshgrid(x,y)
@@ -237,10 +250,12 @@ cg_colors=np.arange(cg_y.size)
 
 print(cg_colors)
 plt.figure()
-plt.scatter(particles[:,0], particles[:,1], c='r',s=2,zorder=2)
-plt.scatter(cg_x, cg_y, c='b',s=3,zorder=3, cmap='Blues')
-
-plt.contour(X,Y,Z, 20,zorder=1)
+plt.scatter(particles[:,0], particles[:,1], c='r',s=2,zorder=2, label='particles')
+plt.scatter(cg_x, cg_y, c='b',s=3,zorder=3, cmap='Blues',label='centroid')
+plt.xlim(mins,maxs)
+plt.ylim(mins,maxs)
+plt.legend()
+plt.contour(X,Y,Z, 14,zorder=1)
 
 # plot the final contour and particles in a 3d plot
 fig = plt.figure()
@@ -248,15 +263,18 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none', alpha=0.5, zorder=1)
 ax.scatter(particles[:,0], particles[:,1], f(particles[:,0], particles[:,1]), c='r',s=2,zorder=2)
 ax.scatter(cg_x, cg_y, cg_z, c='b',s=3,zorder=3, cmap='Blues')
-ax.set_xlim(-20000,20000)
-ax.set_ylim(-20000,20000)
+ax.set_xlim(mins,maxs)
+ax.set_ylim(mins,maxs)
 
 
 plt.figure()
 plt.plot(np.arange(0,i), areas[:i,0], label='Convex hull area')
 plt.plot(np.arange(0,i), areas[:i,1], label='Smoothed convex hull area')
 plt.grid()
-plt.ylim(0,areas[:,1].max())
+plt
+plt.xlabel('Generation')
+plt.ylabel('Area')
+# plt.ylim(0areas[:,1].max())
 plt.yscale('log')
 plt.show()
 
